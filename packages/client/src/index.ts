4aa1b8fefc426de60691bf539de9a61501934655
ohah <bookyoon173@gmail.com>
2025-12-20 12:14:01 +0900

    style(client): change quotes to single quotes

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -2,6 +2,6 @@
 // 이 파일은 나중에 CDP 프로토콜 구현으로 확장됩니다
 
 export function initCDPClient(serverUrl: string) {
-  console.log("CDP Client initialized", serverUrl);
+  console.log('CDP Client initialized', serverUrl);
   // TODO: WebSocket 연결 및 CDP 프로토콜 구현
 }
