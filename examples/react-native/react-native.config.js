de82c9c35de4310a09b4de90baf4edbc1a1850f2
ohah <bookyoon173@gmail.com>
2026-01-01 23:39:56 +0900

    feat(examples): update React Native example to use Inspector package
    - Update example app to use @ohah/chrome-remote-devtools-react-native package
    - Configure Autolinking for the Inspector package
    - Update App.tsx to use new Inspector API
    - Update Podfile and react-native.config.js for proper integration

--- examples/react-native/react-native.config.js
+++ examples/react-native/react-native.config.js
@@ -1,6 +1,24 @@
 const { withWorkspaceModule } = require('@craby/devkit');
+const path = require('path');
 
 const modulePackagePath = __dirname;
-const config = {};
+const config = {
+  dependencies: {
+    '@ohah/chrome-remote-devtools-react-native': {
+      platforms: {
+        ios: {
+          podspecPath: path.resolve(
+            __dirname,
+            '../../packages/react-native-inspector/ios/ChromeRemoteDevToolsInspector.podspec'
+          ),
+        },
+        android: {
+          sourceDir: path.resolve(__dirname, '../../packages/react-native-inspector/android'),
+          packageImportPath: 'import com.ohah.chromeremotedevtools.ChromeRemoteDevToolsInspectorPackage;',
+        },
+      },
+    },
+  },
+};
 
 module.exports = withWorkspaceModule(config, modulePackagePath);
