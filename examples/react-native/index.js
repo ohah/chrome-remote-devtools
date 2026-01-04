00979370dbc6bff2659947db4e8b97b6370aac70
ohah <bookyoon173@gmail.com>
2026-01-05 04:19:56 +0900

    refactor(example): simplify store files with auto-initialization
    Remove manual setupReduxDevToolsExtension calls from store files
    
    - Update index.js to use auto-initialization
    
    - Simplify Redux store.ts to only import package
    
    - Simplify Zustand stores (Counter, Todo, Cart) to only import package
    
    - All stores now rely on auto-initialization from package import

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -2,11 +2,8 @@
  * @format
  */
 
-// Setup Redux DevTools Extension FIRST, before any stores are imported / store import 전에 먼저 Redux DevTools Extension 설정
-// This is critical because Zustand/Redux stores check for extension during module initialization / 이것은 중요합니다. Zustand/Redux store가 모듈 초기화 중에 extension을 확인하기 때문입니다
-import { setupReduxDevToolsExtension } from '@ohah/chrome-remote-devtools-react-native';
-console.log('[index.js] Setting up Redux DevTools Extension at entry point...');
-setupReduxDevToolsExtension('localhost', 8080);
+// Note: Redux DevTools Extension is auto-initialized on import / 참고: Redux DevTools Extension은 import 시 자동 초기화됩니다
+import '@ohah/chrome-remote-devtools-react-native';
 
 import { AppRegistry } from 'react-native';
 import App from './src/App';
