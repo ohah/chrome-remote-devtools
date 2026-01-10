b23f2c994585bce9bb69388921d0d025394b6e27
ohah <bookyoon173@gmail.com>
2026-01-11 02:23:56 +0900

    refactor(examples): remove extension status UI from example app
    - Remove ReduxDevToolsStatus component
    - Remove extension status UI from ReduxCounterScreen
    - Remove extension status UI from ZustandCounterScreen
    - Remove Redux DevTools tab from HomeScreen

--- examples/react-native/src/screens/HomeScreen.tsx
+++ examples/react-native/src/screens/HomeScreen.tsx
@@ -4,7 +4,6 @@ import { View, StyleSheet } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ConsoleTestTab } from '../components/ConsoleTestTab';
 import { NetworkTestTab } from '../components/NetworkTestTab';
-import { ReduxDevToolsStatus } from '../components/ReduxDevToolsStatus';
 import { HookControls } from '../components/HookControls';
 import ChromeRemoteDevToolsInspector from '@ohah/chrome-remote-devtools-react-native';
 
@@ -15,11 +14,6 @@ function HomeTabs() {
     <Tab.Navigator>
       <Tab.Screen name="Console" component={ConsoleTestTab} options={{ title: 'Console Test' }} />
       <Tab.Screen name="Network" component={NetworkTestTab} options={{ title: 'Network Test' }} />
-      <Tab.Screen
-        name="ReduxDevTools"
-        component={ReduxDevToolsStatus}
-        options={{ title: 'Redux DevTools' }}
-      />
     </Tab.Navigator>
   );
 }
