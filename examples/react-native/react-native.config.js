914533caa6d3b70f77cb3ab9e3a74f61d88b857c
ohah <bookyoon173@gmail.com>
2026-02-07 01:30:32 +0900

    chore(examples): drop devkit from react-native.config.js
    - Remove withWorkspaceModule from @craby/devkit; register this package as
      workspace module by reading package.json name and setting dependencies root
    - Keep resolvePackage for symlinked bun monorepo packages (nitro-modules, etc.)

--- examples/react-native/react-native.config.js
+++ examples/react-native/react-native.config.js
@@ -1,8 +1,5 @@
-const { withWorkspaceModule } = require('@craby/devkit');
 const path = require('path');
 const fs = require('fs');
-
-const modulePackagePath = __dirname;
 
 // Resolve symlinks for bun monorepo packages
 const resolvePackage = (packageName) => {
@@ -14,8 +11,12 @@ const resolvePackage = (packageName) => {
   }
 };
 
-const config = {
+// Register this package as a workspace module for autolinking
+const packageName = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).name;
+
+module.exports = {
   dependencies: {
+    [packageName]: { root: __dirname },
     'react-native-nitro-modules': {
       root: resolvePackage('react-native-nitro-modules'),
     },
@@ -24,5 +25,3 @@ const config = {
     },
   },
 };
-
-module.exports = withWorkspaceModule(config, modulePackagePath);
