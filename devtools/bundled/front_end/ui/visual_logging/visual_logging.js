9e4c38505f5409e597794d9a93e07e8ced3a8187
ohah <bookyoon173@gmail.com>
2026-02-11 14:24:07 +0900

    chore(devtools): update bundled to match devtools-frontend clientType cleanup
    - Timeline, elements, React DevTools meta: treat only react-native as RN client
    - Entrypoints (worker_app, devtools_app, trace_app): drop extra client type from conditions
    - Includes other bundled build artifacts (visual_logging)

--- devtools/bundled/front_end/ui/visual_logging/visual_logging.js
+++ devtools/bundled/front_end/ui/visual_logging/visual_logging.js
@@ -3046,6 +3046,7 @@ var knownContextValues = /* @__PURE__ */ new Set([
   "ratechange",
   "raw-headers",
   "raw-headers-show-more",
+  "react-devtools-components",
   "react-devtools-profiler",
   "read-only",
   "read-write",
